4545e68bf63160269abf129055705c093021ea84
Binh Ly Ngoc <binhln@fpt.com>
2025-07-31 18:09:32 +0700

    learn flink

---
+++ deploy/simple_streaming_service/flink_job/deserialize/deserialize_thrift.py
@@ -0,0 +1,172 @@
+import typing
+import json
+from pyflink.table import DataTypes
+from pyflink.table.udf import udtf
+from pyflink.table.types import Row
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TTransport
+import base64
+
+from .structs.ttypes import CollectorPayload
+
+common_schema = DataTypes.ROW([
+    DataTypes.FIELD("schema", DataTypes.STRING()),
+    DataTypes.FIELD("data", DataTypes.MAP(DataTypes.STRING(), DataTypes.STRING())),
+])
+
+co_type = DataTypes.ROW([
+    DataTypes.FIELD("schema", DataTypes.STRING()),
+    DataTypes.FIELD("data", DataTypes.ARRAY(common_schema)),
+])
+
+ue_pr_type = DataTypes.ROW([
+    DataTypes.FIELD("schema", DataTypes.STRING()),
+    DataTypes.FIELD("data", common_schema),
+])
+
+data_row_type = DataTypes.ROW([
+    DataTypes.FIELD("aid", DataTypes.STRING()),
+    DataTypes.FIELD("cd", DataTypes.STRING()),
+    DataTypes.FIELD("cs", DataTypes.STRING()),
+    DataTypes.FIELD("cx", co_type),
+    DataTypes.FIELD("ds", DataTypes.STRING()),
+    DataTypes.FIELD("uid", DataTypes.STRING()),
+    DataTypes.FIELD("dtm", DataTypes.STRING()),
+    DataTypes.FIELD("duid", DataTypes.STRING()),
+    DataTypes.FIELD("e", DataTypes.STRING()),
+    DataTypes.FIELD("eid", DataTypes.STRING()),
+    DataTypes.FIELD("lang", DataTypes.STRING()),
+    DataTypes.FIELD("p", DataTypes.STRING()),
+    DataTypes.FIELD("ue_px", ue_pr_type),
+    DataTypes.FIELD("page", DataTypes.STRING()),
+    DataTypes.FIELD("refr", DataTypes.STRING()),
+    DataTypes.FIELD("res", DataTypes.STRING()),
+    DataTypes.FIELD("sid", DataTypes.STRING()),
+    DataTypes.FIELD("stm", DataTypes.STRING()),
+    DataTypes.FIELD("tna", DataTypes.STRING()),
+    DataTypes.FIELD("tv", DataTypes.STRING()),
+    DataTypes.FIELD("tz", DataTypes.STRING()),
+    DataTypes.FIELD("url", DataTypes.STRING()),
+    DataTypes.FIELD("vid", DataTypes.STRING()),
+    DataTypes.FIELD("vp", DataTypes.STRING()),
+    DataTypes.FIELD("ue_pr", ue_pr_type),
+    DataTypes.FIELD("co", co_type)
+])
+
+body_type = DataTypes.ROW([
+    DataTypes.FIELD("data", DataTypes.ARRAY(data_row_type))
+])
+
+result_type = DataTypes.ROW([
+    DataTypes.FIELD("schema_str", DataTypes.STRING()),
+    DataTypes.FIELD("ipAddress", DataTypes.STRING()),
+    DataTypes.FIELD("timestamp", DataTypes.BIGINT()),
+    DataTypes.FIELD("encoding", DataTypes.STRING()),
+    DataTypes.FIELD("collector", DataTypes.STRING()),
+    DataTypes.FIELD("userAgent", DataTypes.STRING()),
+    DataTypes.FIELD("refererUri", DataTypes.STRING()),
+    DataTypes.FIELD("path", DataTypes.STRING()),
+    DataTypes.FIELD("querystring", DataTypes.STRING()),
+    DataTypes.FIELD("body", body_type),
+    DataTypes.FIELD("headers", DataTypes.ARRAY(DataTypes.STRING())),
+    DataTypes.FIELD("contentType", DataTypes.STRING()),
+    DataTypes.FIELD("hostname", DataTypes.STRING()),
+    DataTypes.FIELD("networkUserId", DataTypes.STRING())
+])
+
+def parse_ue_pr(json_obj) -> Row:
+    json_obj = json.loads(json_obj)
+    data = json_obj.get("data")
+    data_map = {k: str(v) for k, v in data.items()} if isinstance(data, dict) else {}
+
+    return Row(json_obj.get("schema"), data_map)
+
+def parse_co(json_obj) -> Row:
+    json_obj = json.loads(json_obj)
+    data_array = []
+    for item in json_obj["data"]:
+        schema = item.get("schema")
+        data = item.get("data")
+        data_map = {k: str(v) for k, v in data.items()} if isinstance(data, dict) else {}
+        data_array.append(Row(schema, data_map))
+
+    return Row(json_obj.get("schema"), data_array)
+
+@udtf(result_types=result_type)
+def deserialize_collector_payload(data: bytes) -> typing.Iterable[Row]:
+    """
+    A User-Defined Table Function (UDTF) that deserializes a byte array
+    into a structured Row representing a CollectorPayload object.
+    """
+
+    if not data:
+        return
+
+    try:
+        transport = TTransport.TMemoryBuffer(data)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        payload = CollectorPayload()
+        payload.read(protocol)
+
+        body_obj = None
+        if payload.body:
+            try:
+                body_dict = json.loads(payload.body)
+                if "data" in body_dict and isinstance(body_dict["data"], list):
+                    structured_data = []
+                    for item in body_dict["data"]:
+                        ue_px = base64.b64decode(item.get("ue_px")).decode('utf-8')
+                        cx = base64.b64decode(item.get("cx")).decode('utf-8')
+
+                        structured_data.append(Row(
+                            item.get("aid"),
+                            item.get("cd"),
+                            item.get("cs"),
+                            parse_co(cx),
+                            item.get("ds"),
+                            item.get("uid"),
+                            item.get("dtm"),
+                            item.get("duid"),
+                            item.get("e"),
+                            item.get("eid"),
+                            item.get("lang"),
+                            item.get("p"),
+                            parse_ue_pr(ue_px),
+                            item.get("page"),
+                            item.get("refr"),
+                            item.get("res"),
+                            item.get("sid"),
+                            item.get("stm"),
+                            item.get("tna"),
+                            item.get("tv"),
+                            item.get("tz"),
+                            item.get("url"),
+                            item.get("vid"),
+                            item.get("vp"),
+                            parse_ue_pr(item.get("ue_pr")),
+                            parse_co(item.get("co"))
+                        ))
+                    body_obj = Row(structured_data)
+            except Exception as e:
+                pass
+
+        yield Row(
+            payload.schema,
+            payload.ipAddress,
+            payload.timestamp,
+            payload.encoding,
+            payload.collector,
+            payload.userAgent,
+            payload.refererUri,
+            payload.path,
+            payload.querystring,
+            body_obj,
+            payload.headers,
+            payload.contentType,
+            payload.hostname,
+            payload.networkUserId
+        )
+
+    except Exception as e:
+        pass
--- deploy/simple_streaming_service/flink_job/job.py
+++ deploy/simple_streaming_service/flink_job/job.py
@@ -1,19 +1,25 @@
 import os
-import glob
 from pyflink.table import EnvironmentSettings, TableEnvironment
+
 
 def main():
     env_settings = EnvironmentSettings.in_streaming_mode()
     t_env = TableEnvironment.create(env_settings)
     t_env.get_config().get_configuration().set_string("execution.checkpointing.interval", "60s")
     t_env.get_config().get_configuration().set_string("execution.checkpointing.mode", "EXACTLY_ONCE")
-#     jar_path = r"file:///C:/Users/binhln/Documents/work-space/code4future/deploy/simple_streaming_service/flink_job/jars/flink-sql-connector-kafka-3.3.0-1.20.jar;file:///C:/Users/binhln/Documents/work-space/code4future/deploy/simple_streaming_service/flink_job/jars/flink-connector-jdbc-3.3.0-1.20.jar;file:///C:/Users/binhln/Documents/work-space/code4future/deploy/simple_streaming_service/flink_job/jars/mysql-connector-java-8.0.30.jar;file:///C:/Users/binhln/Documents/work-space/code4future/deploy/simple_streaming_service/flink_job/jars/flink-connector-starrocks-1.2.11_flink-1.20.jar"
 
+    job_dir = os.path.dirname(os.path.realpath(__file__))
+    deserialize_module_path = os.path.join(job_dir, "deserialize")
+    t_env.get_config().set("python.files", "deserialize/")
+    t_env.get_config().set("python.files", f"file://{deserialize_module_path}")
+    from deserialize.deserialize_thrift import deserialize_collector_payload
+    t_env.create_temporary_system_function("DeserializeCollector", deserialize_collector_payload)
+#     jar_path = r"file:///C:/Users/binhln/Documents/work-space/code4future/deploy/simple_streaming_service/flink_job/jars/flink-sql-connector-kafka-3.3.0-1.20.jar;"
 #     t_env.get_config().set("pipeline.jars", jar_path)
 
     t_env.execute_sql("""
         CREATE TABLE kafka_source (
-            `value` STRING
+            `value` BYTES
         ) WITH (
             'connector' = 'kafka',
             'topic' = 'snowplow.good.events.1000',
@@ -24,41 +30,122 @@ def main():
         )
     """)
 
-    # ✅ Define StarRocks Sink Table (via JDBC)
     t_env.execute_sql("""
-        CREATE TABLE starrocks_sink (
-               mess STRING
-        ) WITH (
-            'connector' = 'starrocks',
-            'jdbc-url' = 'jdbc:mysql://starrocks-fe:9030',
-            'load-url' = 'starrocks-fe:8030',
-            'database-name' = 'test_db',
-            'table-name' = 'snowplow_events',
-            'username' = 'root',
-            'password' = '',
-            'sink.buffer-flush.interval-ms' = '1000'
-            )
+        CREATE TEMPORARY VIEW events_with_body AS
+        SELECT
+            schema_str,
+            ipAddress,
+            `timestamp`,
+            encoding,
+            collector,
+            userAgent,
+            refererUri,
+            path,
+            querystring,
+            body,
+            headers,
+            contentType,
+            hostname,
+            networkUserId
+        FROM
+            kafka_source,
+            LATERAL TABLE(DeserializeCollector(`value`))
+            AS p(schema_str, ipAddress, `timestamp`, encoding, collector, userAgent, refererUri,
+                 path, querystring, body, headers, contentType, hostname, networkUserId)
     """)
 
+    # table = t_env.from_path("events_with_body")
+    # table.print_schema()
+
     t_env.execute_sql("""
-        INSERT INTO starrocks_sink
-        SELECT `value` as mess FROM kafka_source
-    """).wait()
+        CREATE TEMPORARY VIEW structured_events AS
+        SELECT
+            schema_str,
+            ipAddress,
+            TO_TIMESTAMP_LTZ(`timestamp`, 3) AS server_time,
+            encoding,
+            collector,
+            userAgent,
+            refererUri,
+            path,
+            querystring,
+            body_data.cx,
+            headers,
+            contentType,
+            hostname,
+            networkUserId
+        FROM events_with_body e
+        CROSS JOIN UNNEST(e.body.data) AS body_data
+    """)
+
+    # t_env.execute_sql("""
+    #     CREATE TEMPORARY VIEW transform AS
+    #     SELECT
+    #         cx
+    #     FROM structured_events
+    # """)
+
+
+# ✅ Define StarRocks Sink Table (via JDBC)
+#     t_env.execute_sql("""
+#         CREATE TABLE starrocks_sink (
+#                mess STRING
+#         ) WITH (
+#             'connector' = 'starrocks',
+#             'jdbc-url' = 'jdbc:mysql://starrocks-fe:9030',
+#             'load-url' = 'starrocks-fe:8030',
+#             'database-name' = 'test_db',
+#             'table-name' = 'snowplow_events',
+#             'username' = 'root',
+#             'password' = '',
+#             'sink.buffer-flush.interval-ms' = '1000'
+#             )
+#     """)
+
+    # t_env.execute_sql("""
+    #     INSERT INTO starrocks_sink
+    #     SELECT `value` as mess FROM kafka_source
+    # """).wait()
 
 #     t_env.execute_sql("""
 #         INSERT INTO starrocks_sink VALUES ('ABC'), ('def');
 #     """).wait()
 
-#     t_env.execute_sql("""
-#         CREATE TABLE print_sink (
-#             `value` STRING
-#         ) WITH (
-#             'connector' = 'print'
-#         )
-#     """)
-#
-#     print("Job starting... reading from Kafka and printing to console.")
-#     t_env.execute_sql("INSERT INTO print_sink SELECT * FROM kafka_source").wait()
+    # t_env.execute_sql("""
+    #     CREATE TABLE print_sink (
+    #         schema_str STRING,
+    #         ipAddress STRING,
+    #         `timestamp` TIMESTAMP,
+    #         encoding STRING,
+    #         collector STRING,
+    #         userAgent STRING,
+    #         refererUri STRING,
+    #         path STRING,
+    #         querystring STRING,
+    #         body ROW<data ARRAY<ROW<aid STRING, cd STRING, cs STRING, cx STRING, ds STRING, uid STRING,
+    #             dtm STRING, duid STRING, e STRING, eid STRING, lang STRING, p STRING,
+    #             ue_px STRING, page STRING, refr STRING, res STRING, sid STRING,
+    #             stm STRING, tna STRING, tv STRING, tz STRING, url STRING, vid STRING,
+    #             vp STRING, ue_pr STRING, co STRING>>>,
+    #         headers ARRAY<STRING>,
+    #         contentType STRING,
+    #         hostname STRING,
+    #         networkUserId STRING
+    #     ) WITH (
+    #         'connector' = 'print'
+    #     )
+    # """)
+
+    t_env.execute_sql("""
+      CREATE TABLE print_sink1 (
+          cx ROW<schema STRING,data ARRAY<ROW<schema STRING, data map<string,string>>>>
+      ) WITH (
+            'connector' = 'print'
+            )
+      """)
+
+    print("Job starting... reading from Kafka and printing to console.")
+    t_env.execute_sql("""INSERT INTO print_sink1 SELECT   cx  FROM structured_events""").wait()
 
 if __name__ == "__main__":
     main()
